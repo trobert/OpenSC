0dd2a215a54855db1977f1901157b84027abf8ce
Thibaut Robert <thibaut.robert@worldline.com>
2018-06-25 21:14:38 +0200

    sid800: add all certificates

--- src/libopensc/pkcs15-sid800.c
+++ src/libopensc/pkcs15-sid800.c
@@ -19,28 +19,61 @@
 
 #include "internal.h"
 #include "pkcs15.h"
+#include "compression.h"
 
 static struct sc_aid pub_container = {
 	{0xA0,0x00,0x00,0x00,0x63,0x86,0x02,0x00,0x06}, 9};
 
 static int sc_pkcs15emu_sid800_init(sc_pkcs15_card_t *p15card)
 {
-	u8 buf[1024];
+	u8 buf[4096];
+	u8* certfile = NULL;
+	size_t compressed_len, certfile_len;
+	int r, file_id;
+	
 	sc_card_t *card = p15card->card;
 	sc_path_t path = {
-	  {0, 0}, 0, 0, 0, SC_PATH_TYPE_FILE_ID, pub_container};
+	  {0, 0}, 0, 0, -1, SC_PATH_TYPE_FILE_ID, pub_container};
+
+	sc_pkcs15_cert_info_t cert_info;
+	sc_pkcs15_object_t cert_obj;
 	
 	SC_FUNC_CALLED(card->ctx, SC_LOG_DEBUG_VERBOSE);
+
+	p15card->opts.use_file_cache = 1;	
+
 	if (p15card->tokeninfo->label != NULL)
 		free(p15card->tokeninfo->label);
 	p15card->tokeninfo->label = strdup("SecurID (sid800)");
+	if (p15card->tokeninfo->serial_number != NULL)
+		free(p15card->tokeninfo->serial_number);
+	p15card->tokeninfo->serial_number = strdup("1234");
 	if (p15card->tokeninfo->manufacturer_id != NULL)
 		free(p15card->tokeninfo->manufacturer_id);
 	p15card->tokeninfo->manufacturer_id = strdup("RSA");
 
-	sc_append_file_id(&path, 0x6331);
-	sc_select_file(card, &path, NULL);
-	sc_read_binary(card, 0, buf, 1024, 0);
+	for (file_id = 0x6331; file_id<=0x6337; file_id++) {
+		path.len = 0;
+		sc_append_file_id(&path, file_id);
+		sc_select_file(card, &path, NULL);
+		r = sc_read_binary(card, 0, buf, 4096, 0);
+		if (r <= 0)
+			continue;
+		
+		compressed_len = r;
+		sc_decompress_alloc(&certfile, &certfile_len,
+				    buf, compressed_len, COMPRESSION_ZLIB);
+
+		memset(&cert_info, 0, sizeof(cert_info));
+		memset(&cert_obj,  0, sizeof(cert_obj));
+
+		cert_info.id.value[0] = file_id - 0x6331;
+		cert_info.id.len = 1;
+		cert_info.path = path;
+
+		sc_pkcs15_cache_file(p15card, &path, certfile, certfile_len);
+		sc_pkcs15emu_add_x509_cert(p15card, &cert_obj, &cert_info);
+	}
   	LOG_FUNC_RETURN(card->ctx, SC_SUCCESS);
 }
 
