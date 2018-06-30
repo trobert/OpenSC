ece654b119962947a67beae6055c26e195c0a957
Thibaut Robert <thibaut.robert@worldline.com>
2018-06-30 17:39:13 +0200

    sid800: add signature

--- src/libopensc/pkcs15-sid800.c
+++ src/libopensc/pkcs15-sid800.c
@@ -106,7 +106,7 @@ static int sc_pkcs15emu_sid800_init(sc_pkcs15_card_t *p15card)
 		prkey_info.path.aid = pki_aid;
 		prkey_info.path.aid.value[7] = file_id - 0x7030;
 		prkey_info.path.len = 0;
-		prkey_info.usage = SC_PKCS15_PRKEY_USAGE_SIGN;
+		prkey_info.usage = SC_PKCS15_PRKEY_USAGE_SIGN | SC_PKCS15_PRKEY_USAGE_NONREPUDIATION;
 		prkey_info.native = 1;
 		prkey_info.modulus_length = 2048; // arbitrary value...
 		pkcs15_obj.auth_id.len = 1;
